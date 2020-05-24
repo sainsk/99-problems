78289314bc111cb42b695cbcfe41f4ab23715601
sainsk <kiran.sanni@gmail.com>
2020-05-24 09:29:26 +0530

    Rename Packaging

---
+++ java8/src/main/java/com/saikiran/ninetynine_problems/_02_arithmetic/P41.java
@@ -0,0 +1,8 @@
+package com.saikiran.ninetynine_problems._02_arithmetic;
+
+/**
+ * (*) Compare the two methods of calculating Euler's totient function.
+ * Check P41Test
+ */
+public class P41 {
+}
